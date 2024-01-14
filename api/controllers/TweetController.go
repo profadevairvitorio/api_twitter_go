19ea81a79fbf80ca68ea76e59c896151ba9abe89
profadevairvitorio <profadevairvitorio@gmail.com>
2024-01-14 00:28:06 -0300

    chore: create tweet controller

---
+++ api/controllers/TweetController.go
@@ -0,0 +1,19 @@
+package controllers
+
+import (
+	"api/api/entities"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type tweetController struct {
+	tweets []entities.Tweet
+}
+
+func NewTweetController() *tweetController {
+	return &tweetController{}
+}
+
+func (t *tweetController) findAll(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, t.tweets)
+}
